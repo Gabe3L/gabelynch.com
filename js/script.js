7df696a3c2c34b8dffd06bfabb8eb6c3d9b392b8
Gabe3L <gabe3lynch@gmail.com>
2025-04-23 11:01:00 -0400

    Cleanup of JS and CSS

--- js/script.js
+++ js/script.js
@@ -1,176 +1,53 @@
-AOS.init({
-	duration: 800,
-	easing: 'slide'
-});
+// Sitewide Animations
 
 (function ($) {
-
-	"use strict";
-
-	$(window).stellar({
-		responsive: true,
-		parallaxBackgrounds: true,
-		parallaxElements: true,
-		horizontalScrolling: false,
-		hideDistantElements: false,
-		scrollProperty: 'scroll'
-	});
-
-
-	var fullHeight = function () {
-
-		$('.js-fullheight').css('height', $(window).height());
-		$(window).resize(function () {
-			$('.js-fullheight').css('height', $(window).height());
-		});
-
-	};
-	fullHeight();
-
-	var loader = function () {
-		setTimeout(function () {
-			if ($('#ftco-loader').length > 0) {
-				$('#ftco-loader').removeClass('show');
-			}
-		}, 1);
-	};
-	loader();
-
-	$.Scrollax();
-	
-	var burgerMenu = function () {
-		$('body').on('click', '.js-fh5co-nav-toggle', function (event) {
-			event.preventDefault();
-
-			if ($('#ftco-nav').is(':visible')) {
-				$(this).removeClass('active');
-			} else {
-				$(this).addClass('active');
-			}
-		});
-	};
-
-	burgerMenu();
-
-	var onePageClick = function () {
-		$(document).on('click', '#ftco-nav a[href^="#"]', function (event) {
-			event.preventDefault();
-
-			$('html, body').animate({
-				scrollTop: $($.attr(this, 'href')).offset().top - 70
-			}, 500, function () {});
-		});
-
-	};
-
-	onePageClick();
-
-	$('nav .dropdown').hover(function () {
-		var $this = $(this);
-		$this.addClass('show');
-		$this.find('> a').attr('aria-expanded', true);
-		$this.find('.dropdown-menu').addClass('show');
-	}, function () {
-		var $this = $(this);
-		$this.removeClass('show');
-		$this.find('> a').attr('aria-expanded', false);
-		$this.find('.dropdown-menu').removeClass('show');
-	});
-
-	var scrollWindow = function () {
-		$(window).scroll(function () {
-			var $w = $(this),
-				st = $w.scrollTop(),
-				navbar = $('.ftco_navbar'),
-				sd = $('.js-scroll-wrap');
-
-			if (st > 150) {
-				if (!navbar.hasClass('scrolled')) {
-					navbar.addClass('scrolled');
-				}
-			}
-			if (st < 150) {
-				if (navbar.hasClass('scrolled')) {
-					navbar.removeClass('scrolled sleep');
-				}
-			}
-			if (st > 350) {
-				if (!navbar.hasClass('awake')) {
-					navbar.addClass('awake');
-				}
-
-				if (sd.length > 0) {
-					sd.addClass('sleep');
-				}
-			}
-			if (st < 350) {
-				if (navbar.hasClass('awake')) {
-					navbar.removeClass('awake');
-					navbar.addClass('sleep');
-				}
-				if (sd.length > 0) {
-					sd.removeClass('sleep');
-				}
-			}
-		});
-	};
-	scrollWindow();
-
-	var counter = function () {
-		$('#section-counter, .hero-wrap, .ftco-counter, .ftco-about').waypoint(function (direction) {
-
-			if (direction === 'down' && !$(this.element).hasClass('ftco-animated')) {
-				var comma_separator_number_step = $.animateNumber.numberStepFactories.separator(',')
-				$('.number').each(function () {
-					var $this = $(this),
-						num = $this.data('number');
-					$this.animateNumber(
-						{
-							number: num,
-							numberStep: comma_separator_number_step
-						}, 7000
-					);
-				});
-			}
-		}, { offset: '95%' });
-
-	}
-	counter();
-
-	var contentWayPoint = function () {
-		var i = 0;
+    function initializeScrollAnimations() {
 		$('.ftco-animate').waypoint(function (direction) {
+            const element = $(this.element);
 
-			if (direction === 'down' && !$(this.element).hasClass('ftco-animated')) {
-
-				i++;
-
-				$(this.element).addClass('item-animate');
+			if (direction === 'down' && !element.hasClass('ftco-animated')) {
+				element.addClass('item-animate');
 				setTimeout(function () {
+                    $('body .ftco-animate.item-animate').each(function (index) {
+                        const item = $(this);
 
-					$('body .ftco-animate.item-animate').each(function (k) {
-						var el = $(this);
-						setTimeout(function () {
-							var effect = el.data('animate-effect');
-							if (effect === 'fadeIn') {
-								el.addClass('fadeIn ftco-animated');
-							} else if (effect === 'fadeInLeft') {
-								el.addClass('fadeInLeft ftco-animated');
-							} else if (effect === 'fadeInRight') {
-								el.addClass('fadeInRight ftco-animated');
-							} else {
-								el.addClass('fadeInUp ftco-animated');
-							}
-							el.removeClass('item-animate');
-						}, k * 50, 'easeInOutExpo');
-					});
-
-				}, 100);
-
+                        // Stagger each animation by 30ms
+                        setTimeout(function () {
+                            item.addClass('fadeInUp ftco-animated');
+                        }, index * 20);
+                    });
+                }, 100);
 			}
 
-		}, { offset: '95%' });
+        }, {
+            offset: '95%' // Trigger when the element is 95% into the viewport
+        });
 	};
-	contentWayPoint();
+
+    initializeScrollAnimations();
 })(jQuery);
 
+// Technology Buttons
+
+const tabButtons = document.querySelectorAll('.tab-button');
+  
+tabButtons.forEach(button => {
+    button.addEventListener('click', function() {
+        const targetTabId = this.getAttribute('data-tab');
+        const targetContent = document.getElementById(targetTabId);
+
+        document.querySelectorAll('.tab-content').forEach(content => {
+            content.style.display = 'none';
+            content.classList.remove('active');
+        });
+
+        tabButtons.forEach(btn => btn.classList.remove('active'));
+
+        if (targetContent) {
+            targetContent.style.display = 'flex';
+            targetContent.classList.add('active');
+        }
+
+        this.classList.add('active');
+    });
+});
